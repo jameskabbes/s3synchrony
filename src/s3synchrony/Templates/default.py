bbd89df80181d75930dd8276ffcce202689e36d6
James Kabbes <james.kabbes@gmail.com>
2022-04-28 18:42:05 -0500

    changing templates to outsource smart_sync() call

--- src/s3synchrony/Templates/default.py
+++ src/s3synchrony/Templates/default.py
@@ -1,15 +1,11 @@
-import s3synchrony
-from s3synchrony import smart_sync 
 import aws_credentials
 import user_profile
 
-def run( sync_params, *sys_args ):
+def get_params( sync_params, *sys_args ):
 
     sync_params['_name'] = user_profile.profile.name
 
     aws_role = user_profile.profile.aws_roles[ sync_params['aws_role_shorthand'] ]
     sync_params['credentials'] = aws_credentials.Creds[ aws_role ].dict
-
-    print (sync_params)
-    smart_sync( **sync_params )
-    
+  
+    return sync_params
